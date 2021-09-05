e155fd00df1fe3005a017548904e6e1ab84db31c
Trần Công <nopain1210@gmail.com>
2021-09-05 16:40:10 +0700

    Format code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,9 +6,9 @@ import '@openzeppelin/hardhat-upgrades';
 
 import { HardhatUserConfig, task } from 'hardhat/config';
 
-import { config as dotEnvConfig } from "dotenv";
+import { config as dotEnvConfig } from 'dotenv';
 
-dotEnvConfig()
+dotEnvConfig();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,7 +20,7 @@ task('accounts', 'Prints the list of accounts', async (args, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const {API_URL, PRIVATE_KEY} = process.env
+const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -30,9 +30,9 @@ const config: HardhatUserConfig = {
     hardhat: {},
     rinkeby: {
       url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
