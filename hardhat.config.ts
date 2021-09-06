201223c7594519b794767d8f2ffef212fe6d8d32
Trần Công <nopain1210@gmail.com>
2021-09-06 17:11:28 +0700

    Upgrade contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,14 +4,16 @@ import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 import '@openzeppelin/hardhat-upgrades';
 import 'hardhat-abi-exporter';
-
 import { HardhatUserConfig } from 'hardhat/config';
-
 import { config as dotEnvConfig } from 'dotenv';
+import { cleanEnv, str } from 'envalid';
 
 dotEnvConfig();
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY } = cleanEnv(process.env, {
+  API_URL: str(),
+  PRIVATE_KEY: str(),
+});
 
 const config: HardhatUserConfig = {
   solidity: {
